ad99f8539e14423e6100a59d55c49e294de9025a
Ivan Ortega <ivanortegaalba@gmail.com>
2023-04-21 17:22:00 +0200

    Add analytics to modal setting a src

--- public/app/features/datasources/components/picker/DataSourceDropdown.tsx
+++ public/app/features/datasources/components/picker/DataSourceDropdown.tsx
@@ -174,6 +174,7 @@ const PickerContent = React.forwardRef<HTMLDivElement, PickerContentProps>((prop
                 showModal(DataSourceModal, {
                   enableFileUpload: props.enableFileUpload,
                   fileUploadOptions: props.fileUploadOptions,
+                  reportedInteractionFrom: 'ds_picker',
                   current,
                   onDismiss: hideModal,
                   onChange: (ds) => {
